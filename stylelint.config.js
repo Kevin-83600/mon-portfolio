c986deb75c56b6ac4de964e006c67c9dd0b46051
kevingit config --global user.email <kevinwlosniewski@mode83.onmicrosoft.com>
2025-08-20 13:19:55 +0200

    mise a jour et correction pipeline

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,13 +1,7 @@
 module.exports = {
-  extends: "stylelint-config-standard",
+  extends: ["stylelint-config-standard"],
 
   rules: {
     "block-no-empty": true,
-
-    "color-no-invalid-hex": true,
-
-    "declaration-block-no-duplicate-properties": true,
-
-    "no-empty-source": true,
   },
 };
