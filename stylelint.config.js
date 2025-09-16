07293a5f9a413ca446533b2e4051a387d7f45a3d
kevingit config --global user.email <kevinwlosniewski@mode83.onmicrosoft.com>
2025-09-16 10:53:07 +0200

    MAJ

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,11 +3,11 @@ module.exports = {
 
   rules: {
     "block-no-empty": true,
-
     "color-no-invalid-hex": true,
-
     "declaration-block-no-duplicate-properties": true,
-
     "no-empty-source": true,
+
+    // 🔽 Ajout de cette ligne pour désactiver le warning des lignes vides
+    "rule-empty-line-before": null,
   },
 };
